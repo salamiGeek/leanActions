7bcea4f2d04921db5c9d70b25580463bd77de415
Yarnell <mryarnell@foxmail.com>
2021-06-18 21:58:21 +0800

    添加了kenzok8/openwrt-packages的仓库

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,10 +10,7 @@
 
 # Uncomment a feed source
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# trojan
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-# adguardHome
-sed -i '$a src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' feeds.conf.default
