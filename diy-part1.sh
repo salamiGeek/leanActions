989014b793a8ff0fa8836e21450d1a3c56fdad49
Yarnell <mryarnell@foxmail.com>
2021-06-18 11:16:24 +0800

    修复因命名错误导致编译不通过

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@ sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.
 
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # adguardHome
-sed -i '$a src-git small https://github.com/rufengsuixing/luci-app-adguardhome' feeds.conf.default
+sed -i '$a src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' feeds.conf.default
