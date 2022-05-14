506892ec5097155fc539391f2e0d7b39615232eb
mryarnell <mryarnell@foxmail.com>
2022-05-14 13:22:16 +0800

    修复diy-part1.sh中的错误

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,4 @@ sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 # trojan
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # NanoHatOLED
-sed -i 'a src-git NanoHatOLED https://github.com/vinewx/NanoHatOLED.git' feeds.conf.default
+sed -i '$a src-git NanoHatOLED https://github.com/vinewx/NanoHatOLED.git' feeds.conf.default
