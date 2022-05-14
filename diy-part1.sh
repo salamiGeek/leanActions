1549f1debaff99a0dda7b6097b80abd704a96e93
mryarnell <mryarnell@foxmail.com>
2022-05-14 13:10:44 +0800

    添加NanoHatOLED

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,3 +14,5 @@ sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.de
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # trojan
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# NanoHatOLED
+sed -i 'a src-git NanoHatOLED https://github.com/vinewx/NanoHatOLED.git' feeds.conf.default
